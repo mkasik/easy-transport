679a8186bb245f9cb872a02cba038a495f66413f
chamokBarmon <chamokbarmon123@gmail.com>
2023-01-13 19:07:52 +0600

    last

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../Banner/Banner.css';
+import "../Banner/Banner.css";
 
 const Banner = () => {
   return (
@@ -8,6 +8,7 @@ const Banner = () => {
         <img
           src="https://img.freepik.com/free-vector/public-city-transport-isometric-flowchart-with-images-different-municipal-private-vehicles-with-text-captions_1284-27299.jpg?w=2000"
           className="w-full"
+          alt=""
         />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">
@@ -19,7 +20,11 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full h">
-        <img src="https://wallpapershome.com/images/wallpapers/ferrari-430-scuderia-3840x2160-2018-cars-autumn-road-5k-16855.jpg" className="w-full" />
+        <img
+          src="https://wallpapershome.com/images/wallpapers/ferrari-430-scuderia-3840x2160-2018-cars-autumn-road-5k-16855.jpg"
+          className="w-full"
+          alt=""
+        />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -30,7 +35,11 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full h">
-        <img src="https://industrytoday.com/wp-content/uploads/2020/12/moving-transporting-heavy-items.jpg" className="w-full" />
+        <img
+          src="https://industrytoday.com/wp-content/uploads/2020/12/moving-transporting-heavy-items.jpg"
+          className="w-full"
+          alt=""
+        />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -41,7 +50,11 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full h">
-        <img src="https://webuyanybike.com/blog/wp-content/uploads/road-g97fb2e486_1920-1200x606.jpg" className="w-full" />
+        <img
+          src="https://webuyanybike.com/blog/wp-content/uploads/road-g97fb2e486_1920-1200x606.jpg"
+          className="w-full"
+          alt=""
+        />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle">
             ❮
--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 
 
